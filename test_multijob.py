1c63f69ff56f9105c962f93ae884f7091f0141bf
Zhihao Lou <lzh1984@gmail.com>
2016-03-24 21:57:50 -0500

    add test of nice

--- test_multijob.py
+++ test_multijob.py
@@ -1,6 +1,11 @@
 import unittest
 import multijob
 import time
+try:
+    import psutil
+    has_psutil = True
+except ImportError:
+    has_psutil = False
 class TestMultiJobTiming(unittest.TestCase):
     def test_timing(self):
         cmds = [
@@ -16,5 +21,15 @@ class TestMultiJobTiming(unittest.TestCase):
         spent = time.time() - start
         self.assertGreater(spent, 0.5)
         self.assertLess(spent, 0.6)
+    @unittest.skipIf(not has_psutil, "psutil not installed")
+    def test_nice(self):
+        cmd = ["python", "-c", "import time; time.sleep(0.1)"]
+        m = multijob.multijob(1,0.1)
+        m.run_job(cmd, nice=10)
+        pid = m.jobs[0].pid
+        p = psutil.Process(pid)
+        print p.nice()
+        self.assertEqual(p.nice(), 10)
+        m.wait_all()
 if __name__ == '__main__':
     unittest.main()
